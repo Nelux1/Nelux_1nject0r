06ded250a2fd637f9b911613c232f64f6acf9371
Nelux1 <72582088+Nelux1@users.noreply.github.com>
2025-05-23 18:16:18 -0300

    Update nelux_injector.py

--- nelux_injector.py
+++ nelux_injector.py
@@ -1,4 +1,4 @@
-import sys
+import sys, re
 import signal
 import argparse
 from injector import test_parameters
@@ -24,7 +24,7 @@ USER_AGENTS = [
 
 # Handle Ctrl+C
 def signal_handler(sig, frame):
-    print(f"\n{RED}[!] Interrupción detectada. Cerrando herramienta...{RESET}")
+    print(f"\n{RED}[!] Interruption detected. Closing tool...{RESET}")
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
@@ -38,13 +38,19 @@ def banner():
 ██║╚██╗██║██╔══╝  ██║     ██║   ██║ ██╔██╗   1NJECT0R
 ██║ ╚████║███████╗███████╗╚██████╔╝██╔╝ ██╗
 ╚═╝  ╚═══╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
-Nelux 1nject0r - Param Filter Checker By Marcos Suarez V2.1
+Nelux 1nject0r - Param Filter Checker By Marcos Suarez V2.0
 {RESET}
     """)
 
 def is_valid_url(url):
     parsed = urlparse(url)
     return parsed.scheme and parsed.netloc
+
+def get_output_filename(url):
+    parsed = urlparse(url)
+    domain = parsed.netloc.replace("www.", "").split(":")[0]  # elimina www. y puertos
+    domain = re.sub(r'\.\w+$', '', domain)  # elimina .com, .net, etc.
+    return f"{domain}_parameters.txt"
 
 def build_headers(args):
     headers = {}
@@ -63,7 +69,7 @@ def build_headers(args):
 
     return headers
 
-def process_url(url, threads, headers):
+def process_url(url, threads, headers, output_filename):
     if not is_valid_url(url):
         print(f"{RED}[!] URL inválida: {url}{RESET}")
         return
@@ -73,7 +79,7 @@ def process_url(url, threads, headers):
         if not urls_con_param:
             print(f"{RED}[!] No injectable parameters found in: {url}{RESET}")
             return
-        test_parameters(urls_con_param, threads, headers)
+        test_parameters(urls_con_param, threads, headers, output_filename)
     except Exception as e:
         print(f"{RED}[!] Error processing the URL {url}: {e}{RESET}")
 
@@ -83,32 +89,41 @@ def main():
     parser.add_argument('-l', '--list', type=str, help="File containing a list of URLs to check")
     parser.add_argument('-t', '--threads', type=int, default=1, help="Number of threads to use (default: 1)")
     parser.add_argument("-w", '--wordlist', dest="word", help="wordlist of payloads", action='store')
-    parser.add_argument("-ra","--random-agent", action="store_true", help="Usar un User-Agent aleatorio")
+    parser.add_argument("-ra","--random-agent", action="store_true", help="Random User-Agent")
     parser.add_argument("-H", "--header", action="append", help="Custom header (can be used multiple times)")
     parser.add_argument('-pl', '--param-list', type=str, help="File with URLs that already contain parameters")
-
 
     args = parser.parse_args()
     banner()
     headers = build_headers(args)
+    
+    if args.header:
+       print(f"{CYAN}[*] Use Headers:{RESET}")
+       print(f"{GREEN}{args.header}{RESET}")
+
+    if args.random_agent:
+        print(f"{CYAN}[*] Use random agent{RESET}")     
 
     if args.url:
-        print(f"{CYAN}[*] Scanning URL: {args.url}{RESET}")
-        process_url(args.url, args.threads, headers)
+        print(f"{CYAN}[*] Scanning URL:{RESET} {args.url}")
+        output_filename = get_output_filename(args.url)
+        process_url(args.url, args.threads, headers, output_filename)
         if args.word:
-            fuzz_from_file(args.word, args.threads,headers)
+            fuzz_from_file(args.word, args.threads, headers)
+
 
     elif args.list:
         try:
             with open(args.list, 'r') as file:
                 urls = file.readlines()
-                print(f"{CYAN}[*] Scanning URLs from file: {args.list}{RESET}")
+                print(f"{CYAN}[*] Scanning URLs from file:{RESET} {args.list}")
                 for url in urls:
                     url = url.strip()
                     if url:
-                        process_url(url, args.threads, headers)
-            if args.word:
-                fuzz_from_file(args.word, args.threads,headers)                                   
+                        output_filename = get_output_filename(url)
+                        process_url(url, args.threads, headers, output_filename)
+                if args.word:
+                    fuzz_from_file(args.word, args.threads, headers)
         except FileNotFoundError:
             print(f"{RED}[!] File not found: {args.list}{RESET}")
             sys.exit(1)
@@ -121,18 +136,20 @@ def main():
             with open(args.param_list, 'r') as file:
                 urls = [line.strip() for line in file if line.strip()]
                 print(f"{CYAN}[*] Testing pre-found parameter URLs from file: {args.param_list}{RESET}")
-                test_parameters(urls, args.threads, headers)
+                for url in urls:
+                    output_filename = get_output_filename(url)
+                    test_parameters([url], args.threads, headers, output_filename)
                 if args.word:
-                    fuzz_from_file(args.word, args.threads,headers)
+                    fuzz_from_file(args.word, args.threads, headers)
         except FileNotFoundError:
             print(f"{RED}[!] File not found: {args.param_list}{RESET}")
             sys.exit(1)
         except Exception as e:
             print(f"{RED}[!] Error reading param list file: {e}{RESET}")
-            sys.exit(1)
-        
+            sys.exit(1)  
     else:
         print(f"{RED}[!] Please specify a URL with -u or a file with -l.{RESET}")
+
 
 if __name__ == "__main__":
     main()
